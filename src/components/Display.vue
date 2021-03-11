14dad90996918ffe8a6fae2ab527c551f408b591
jonathanyji98 <jonathanyji98_28@hotmail.com>
2021-03-11 22:31:37 +1000

    first commit

---
+++ src/components/Display.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import {defineComponent} from "vue";
+
+  export default defineComponent({
+    name: 'Display',
+    
+    setup(){
+      console.log("THE DISPLAY PAGE WORKS");
+    },
+
+    data: function() {
+    return {
+      id: "1",
+      name: "Bob",
+      address: "Unit 12 Yeronga Street, 30000 Queensland",
+      phoneNumber: "+61 256 355 777",
+      time: "8:50pm"
+     };
+    },
+
+    // getData: function(){
+    //   this.id = "1"
+    //   this.name = "Bob"
+    //   this.address = "Unit 12 Yeronga Street, 30000 Queensland"
+    //   this.phoneNumber = "+61 256 355 777"
+    //   this.time = "8:50pm"
+    // }
+
+    });
+    
+</script>
+
+<template>
+    <div class="display">
+      <h4>id: {{id}}</h4> <br>
+      <h4>Name: {{name}}</h4> <br>
+      <h4>Address: {{address}}</h4> <br>
+      <h4>Phone number: {{phoneNumber}}</h4> <br>
+      <h4>Time: {{time}}</h4> <br>
+    </div>
+</template>
+
+<style>
+.display{
+  text-align: left;
+  margin-left: 30%;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <Display></Display>
+  </div>
+</template>
+
+<script lang="ts">
+import Display from '../components/Display.vue'
+
+export default {
+  name: 'About',
+  components: {
+    Display
+  }
+}
+</script>
