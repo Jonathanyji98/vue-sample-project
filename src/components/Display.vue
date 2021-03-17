da65b2d70b4534cbccbb0527c2feeaafa6c51124
jonathanyji98 <jonathanyji98_28@hotmail.com>
2021-03-17 16:39:52 +1000

    methods in progress

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,36 +1,60 @@
 <script lang="ts">
   import {defineComponent} from "vue";
-  //import { OwnerService } from "../../src/services/ownerService.ts";
+  //import { OwnerService } from "../services/ownerService.ts";
+  import PostsService from "../services/PostsService.js"
+
+
 
   export default defineComponent({
     name: 'Display',
-    
-    setup(){
-      console.log("THE DISPLAY PAGE WORKS");
-    },
+
+    // setup(){
+    //   console.log("THE DISPLAY PAGE WORKS");
+    //   this.fetchPosts();
+    // },
+    created() {
+			// this.fetchPosts()
+		},
 
     data: function() {
+      
       return {
-        dataArray: [
-              {
-                id: "1",
-                name: "Bob",
-                address: "Unit 12 Yeronga Street, 30000 Queensland",
-                phoneNumber: "+61 256 355 777",
-                time: "8:50pm"
-              },
-              {
-                id: "2",
-                name: "Andy",
-                address: "Unit 115 Yeronga Street, 30000 Queensland",
-                phoneNumber: "+61 993 445 002",
-                time: "9:50pm"
-              }
-            ]
+        // dataArray: [
+        //       {
+        //         id: "1",
+        //         name: "Bob",
+        //         address: "Unit 12 Yeronga Street, 30000 Queensland",
+        //         phoneNumber: "+61 256 355 777",
+        //         time: "8:50pm"
+        //       },
+        //       {
+        //         id: "2",
+        //         name: "Andy",
+        //         address: "Unit 115 Yeronga Street, 30000 Queensland",
+        //         phoneNumber: "+61 993 445 002",
+        //         time: "9:50pm"
+        //       }
+        //     ]
 
-        // dataArray: OwnerService.getOwnerData()
+        dataArray: [],
+        num: PostsService.getnumber()
       }
-    }
+    },
+
+  
+
+    // methods:{
+    //   fetchPosts() {
+    //     return PostsService.get()
+    //     .then(result =>{
+    //       this.dataArray = result.data
+    //     })
+    //   }
+    // }
+      
+    
+
+
 
 
     });
---
+++ src/services/PostsService.js
@@ -0,0 +1,25 @@
+
+
+export default class PostsService{
+//   get() {
+//     return {
+//         dataArray: [
+//             {
+//               id: "3",
+//               name: "Jack",
+//               address: "Unit 007 Boundry Street, 30000 Queensland",
+//               phoneNumber: "+61 256 355 777",
+//               time: "10:50pm"
+//             },
+//             {
+//               id: "4",
+//               name: "Bean",
+//               address: "Unit 115 New Farm Street, 30000 Queensland",
+//               phoneNumber: "+61 993 445 002",
+//               time: "10:50pm"
+//             }
+//           ]
+//     }
+//   }
+    
+}
