98ab9713ab03c72a5eb2101f3584a6f657348f14
jonathanyji98 <jonathanyji98_28@hotmail.com>
2021-03-17 14:24:32 +1000

    added an array data to loop

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
   import {defineComponent} from "vue";
+  //import { OwnerService } from "../../src/services/ownerService.ts";
 
   export default defineComponent({
     name: 'Display',
@@ -9,34 +10,41 @@
     },
 
     data: function() {
-    return {
-      id: "1",
-      name: "Bob",
-      address: "Unit 12 Yeronga Street, 30000 Queensland",
-      phoneNumber: "+61 256 355 777",
-      time: "8:50pm"
-     };
+      return {
+        dataArray: [
+              {
+                id: "1",
+                name: "Bob",
+                address: "Unit 12 Yeronga Street, 30000 Queensland",
+                phoneNumber: "+61 256 355 777",
+                time: "8:50pm"
+              },
+              {
+                id: "2",
+                name: "Andy",
+                address: "Unit 115 Yeronga Street, 30000 Queensland",
+                phoneNumber: "+61 993 445 002",
+                time: "9:50pm"
+              }
+            ]
+
+        // dataArray: OwnerService.getOwnerData()
+      }
     }
 
-    // getData: function(){
-    //   this.id = "1"
-    //   this.name = "Bob"
-    //   this.address = "Unit 12 Yeronga Street, 30000 Queensland"
-    //   this.phoneNumber = "+61 256 355 777"
-    //   this.time = "8:50pm"
-    // }
 
     });
     
 </script>
 
 <template>
-    <div class="display">
-      <h4>id: {{id}}</h4> <br>
-      <h4>Name: {{name}}</h4> <br>
-      <h4>Address: {{address}}</h4> <br>
-      <h4>Phone number: {{phoneNumber}}</h4> <br>
-      <h4>Time: {{time}}</h4> <br>
+    <div class="display" v-for="entry in dataArray" :key="entry.id">
+      <p><b>id:</b> {{entry.id}}</p>
+      <p><b>Name:</b> {{entry.name}}</p>
+      <p><b>Address:</b> {{entry.address}}</p>
+      <p><b>Phone number:</b> {{entry.phoneNumber}}</p>
+      <p><b>Time:</b> {{entry.time}}</p>
+      <br>
     </div>
 </template>
 
