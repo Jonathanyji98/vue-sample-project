66ea7b54ca32c9711d40df68a275c8350fda992a
jonathanyji98 <jonathanyji98_28@hotmail.com>
2021-03-22 14:54:06 +1000

    http post working

--- Server.js
+++ Server.js
@@ -1,12 +1,34 @@
 //import OwnerService from './src/services/OwnerService.ts'
 
-var OwnerService = require("./src/services/OwnerService.ts")
+// var OwnerService = require("./src/services/OwnerService.ts")
 
 //To start server, type 'node Server.js' into the terminal
 const express = require('express')
 const app = express()
 const port = 3000
 const router = express.Router()
+
+
+var data = [
+    {
+      id: "5",
+      name: "kel",
+      address: "Unit 007 Boundry Street, 30000 Queensland",
+      phoneNumber: "+61 256 355 777",
+      time: "10:50pm"
+    },
+    {
+
+      id: "6",
+      name: "peanut",
+      address: "Unit 115 New Farm Street, 30000 Queensland",
+      phoneNumber: "+61 993 445 002",
+      time: "10:50pm"
+    }
+  ];
+
+
+
 
 app.listen(port, (error) => {
     //If there's an error print error message otherwise print listening on port number
@@ -37,21 +59,26 @@ app.get('/about', (req, res) => {
 
 app.post('/', function(req, res) {
     //Check if all fields are provided and are valid
-    if (!req.body.name || !req.address || !req.phoneNumber || !req.time) {
-        res.status(400)
-        res.json({ message: "Bad Request" })
-    } else {
-        var newId = OwnerService.data.length() + 1
+    // if (!req.body.name || !req.address || !req.phoneNumber || !req.time) {
+    //     res.status(400)
+    //     res.json({ message: "Bad Request" })
+    // } else {
+    //     var newId = OwnerService.data.length() + 1
 
-        OwnerService.data.push({
-            id: newId,
-            name: req.body.name,
-            address: req.body.address,
-            phoneNumber: req.body.phoneNumber,
-            time: req.body.time
-        });
-    }
+    //     OwnerService.data.push({
+    //         id: newId,
+    //         name: req.body.name,
+    //         address: req.body.address,
+    //         phoneNumber: req.body.phoneNumber,
+    //         time: req.body.time
+    //     });
+    // }
 
-    res.json({ message: "New data added" + "ID: " + newId })
+    // res.json({ message: "New data added" + "ID: " + newId })
+
+    console.log(req.body)
+    res.status(200).send('Post Created entry')
+    data.push(req.body)
+    console.log("NEW DATA ADDED CHECK: " + JSON.stringify(data))
 
 })
